94f34fe8bcb3d6fcfd25cb08a8017fc0fea5f39a
Ben Elan <benelan@users.noreply.github.com>
2022-01-12 10:57:05 -0800

    refactor: move App to component directory (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import { defineCustomElements } from "@esri/calcite-components/dist/custom-elements";
 
 defineCustomElements(window);
