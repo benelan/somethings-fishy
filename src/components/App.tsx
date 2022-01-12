06e5a1411ef947958180d6bf47e5f97f8bf10223
Jose Banuelos <36280386+banuelosj@users.noreply.github.com>
2022-01-12 15:17:36 -0800

    adding Swiper with 3 MapViews (#12)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import LazyLoad from "react-lazyload";
 import Map from "./Map";
+import BioMap from "./BioMap";
+import CollectionMap from "./CollectionMap";
+import MapSwiper from "./MapSwiper";
 import Landing from "./landing/Landing";
 import Introduction from "./InfoCards";
 import "@esri/calcite-components/dist/calcite/calcite.css";
 
-const lazyHeight = "70vh";
+const lazyHeight = "90vh";
 
 const ContentContainer = styled.div`
   margin-left: 3rem;
@@ -23,6 +26,16 @@ const MapContainer = styled.div`
   height: ${lazyHeight};
 `;
 
+const SlideContainer = styled.div`
+  height: ${lazyHeight};
+  text-align: center;
+`;
+
+const SlideHeader = styled.div`
+  font-size: 2.5em;
+  color: #237cbd;
+`;
+
 const App: React.FC = (): JSX.Element => {
   return (
     <>
@@ -33,7 +46,20 @@ const App: React.FC = (): JSX.Element => {
         <Introduction />
         <LazyLoad height={lazyHeight} offset={100}>
           <MapContainer>
-            <Map />
+            <MapSwiper>
+              <SlideContainer>
+                <SlideHeader>Seat Temperature Map</SlideHeader>
+                <Map />
+              </SlideContainer>
+              <SlideContainer>
+                <SlideHeader>Biodiversity Map</SlideHeader>
+                <BioMap />
+              </SlideContainer>
+              <SlideContainer>
+                <SlideHeader>Data Collection Map</SlideHeader>
+                <CollectionMap />
+              </SlideContainer>
+            </MapSwiper>
           </MapContainer>
         </LazyLoad>
       </ContentContainer>
---
+++ src/components/BioMap.tsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+//import esriConfig from "@arcgis/core/config";
+import "@arcgis/core/assets/esri/themes/light/main.css";
+
+const MapDiv = styled.div`
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const BioMap: React.FC = (): JSX.Element => {
+  const mapDiv = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      //Add global API key from environment variables to access data
+      // const { REACT_APP_GLOBAL_API_KEY } = process.env;
+      // if (!REACT_APP_GLOBAL_API_KEY) {
+      //   throw new Error("API key not found");
+      // }
+      // esriConfig.apiKey = `${REACT_APP_GLOBAL_API_KEY}`;
+
+      const map = new Map({
+        basemap: "gray-vector"
+      });
+
+      const view = new MapView({
+        container: mapDiv?.current,
+        map,
+        center: [-94, 32],
+        zoom: 4
+      });
+      view.when(() => console.log("loaded biodiversity map..."));
+    }
+  }, [mapDiv]);
+
+  return <MapDiv ref={mapDiv} />;
+};
+
+export default BioMap;
