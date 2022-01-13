c985e678ec4810499a0280245c34fd2e4ef0f619
Ben Elan <benelan@users.noreply.github.com>
2022-01-13 08:45:54 -0800

    refactor(swiper): fix typing and remove anys (#13)

--- src/components/MapSwiper.tsx
+++ src/components/MapSwiper.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
 import { Navigation, Pagination } from "swiper";
 
@@ -7,24 +6,20 @@ import "swiper/swiper.min.css";
 import "swiper/modules/navigation/navigation.min.css";
 import "swiper/modules/pagination/pagination.min.css";
 
-const MapSwiper: React.FC = (props: any): JSX.Element => {
-  const swiperSlides = props.children.map((slide: any, index: number) => {
-    return <SwiperSlide key={index}>{slide}</SwiperSlide>;
-  });
-
-  return (
-    <Swiper
-      allowTouchMove={false}
-      modules={[Navigation, Pagination]}
-      navigation
-      pagination={{ clickable: true }}
-      spaceBetween={20}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <h3>Title</h3>
-      {swiperSlides}
-    </Swiper>
-  );
-};
+const MapSwiper: React.FC = ({ children }): JSX.Element => (
+  <Swiper
+    allowTouchMove={false}
+    modules={[Navigation, Pagination]}
+    navigation
+    pagination={{ clickable: true }}
+    spaceBetween={20}
+    style={{ height: "100%", width: "100%" }}
+  >
+    <h3>Title</h3>
+    {React.Children.map(children, (slide, index) => (
+      <SwiperSlide key={index}>{slide}</SwiperSlide>
+    ))}
+  </Swiper>
+);
 
 export default MapSwiper;
