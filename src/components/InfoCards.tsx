592e89beebaa716ff8279327e158cb3840474f75
Ben Elan <benelan@protonmail.com>
2022-01-16 15:38:32 -0800

    feat(cards): add more sources

--- src/components/InfoCards.tsx
+++ src/components/InfoCards.tsx
@@ -40,7 +40,7 @@ const InfoCards: React.FC = (): JSX.Element => (
         <h3 slot="title">The Problem</h3>
         <ul>
           <ListItem>
-            Global warming may reduce fish and other sea life by 17% by the year 2100.{" "}
+            Global warming may reduce fish and other sea life by 17% by the year 2100.{"   "}
             <Link
               as={CalciteLink}
               href="https://www.pnas.org/content/116/26/12907.short?rss=1"
@@ -51,8 +51,8 @@ const InfoCards: React.FC = (): JSX.Element => (
             </Link>
           </ListItem>
           <ListItem>
-            Over the last decade, the rate of plastic consumption has doubled, increasing by 2.4
-            percent every year.{" "}
+            Over the last decade, the rate of plastic consumption [by fish] has doubled, increasing
+            by 2.4 percent every year.{"   "}
             <Link
               as={CalciteLink}
               href="https://news.stanford.edu/2021/02/09/plastic-ingestion-fish-growing-problem/"
@@ -64,12 +64,23 @@ const InfoCards: React.FC = (): JSX.Element => (
           </ListItem>
           <ListItem>
             386 marine fish species are known to have ingested plastic debris, including 210 species
-            that are commercially important.{" "}
+            that are commercially important.{"   "}
             <Link
               as={CalciteLink}
-              href="https://www.civilbeat.org/2021/03/the-fish-we-eat-are-eating-plastic-thats-not-a-good-thing/"
+              href="https://onlinelibrary.wiley.com/doi/full/10.1111/gcb.15533"
               rel="noopener noreferrer"
-              style={{ "--calcite-font-size--2": "0.1em" }}
+              target="_blank"
+            >
+              source
+            </Link>
+          </ListItem>
+          <ListItem>
+            Ocean pollution is a widespread, worsening, and poorly controlled problem that is
+            directly affecting human and ecosystem health.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.niehs.nih.gov/research/programs/geh/geh_newsletter/2021/2/articles/new_study_finds_ocean_pollution_a_threat_to_human_health.cfm"
+              rel="noopener noreferrer"
               target="_blank"
             >
               source
@@ -82,12 +93,50 @@ const InfoCards: React.FC = (): JSX.Element => (
       <Card as={CalciteCard}>
         <h3 slot="title">The Solutions</h3>
         <ul>
-          <ListItem>Buy local, sustainable food and products.</ListItem>
-          <ListItem>Carry a reusable bottle instead of buying single-use plastics.</ListItem>
-          <ListItem>Volunteer your time to pickup trash at a local beach.</ListItem>
           <ListItem>
-            Ask your representatives to create more protected areas to save endangered fish
-            populations.
+            Buy local, sustainable fish when possible.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.seafoodwatch.org/recommendations/search?query="
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </ListItem>
+          <ListItem>
+            Carry a reusable bottle instead of buying single-use plastics.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://blogs.nicholas.duke.edu/env212/single-use-plastic-and-its-effects-on-our-oceans"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </ListItem>
+          <ListItem>
+            Volunteer your time to pickup trash at a local beach.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://oceanconservancy.org/trash-free-seas/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </ListItem>
+          <ListItem>
+            Ask your representatives to create and fund more Marine Protected Areas to save
+            endangered aquatic ecosystems.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.americanprogress.org/article/marine-protected-areas-help-fisheries-ocean-ecosystems/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
           </ListItem>
         </ul>
       </Card>
