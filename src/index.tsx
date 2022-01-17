46d400a9fe9c17b106972761fe410f837a832875
Ben Elan <benelan@users.noreply.github.com>
2022-01-16 19:13:04 -0800

    fix(icons): use calcite assets hosted on CDN (#24)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+setAssetPath(`https://unpkg.com/@esri/calcite-components/dist/calcite/assets`);
 
 ReactDOM.render(
   <React.StrictMode>
