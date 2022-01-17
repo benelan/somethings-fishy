a362d1c063cdfa75295b7c44ac11fc2688e9f763
Ben Elan <benelan@protonmail.com>
2022-01-17 01:52:27 -0800

    fix: unnest styled components

--- src/components/maps/MapSwiperDeck.tsx
+++ src/components/maps/MapSwiperDeck.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+import { Navigation, Pagination } from "swiper";
 import "@esri/calcite-components/dist/components/calcite-button";
 import "@esri/calcite-components/dist/components/calcite-modal";
 import { CalciteButton, CalciteModal } from "@esri/calcite-components-react";
@@ -7,46 +9,44 @@ import BioMap from "./BioMap";
 import RarityMap from "./RarityMap";
 import CollectionMap from "./CollectionMap";
 import Map from "./Map";
-import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
-import { Navigation, Pagination } from "swiper";
 import ErrorBoundary from "../ErrorBoundary";
-import "@arcgis/core/assets/esri/themes/light/main.css";
 import "swiper/swiper.min.css";
 import "swiper/modules/navigation/navigation.min.css";
 import "swiper/modules/pagination/pagination.min.css";
+import "@arcgis/core/assets/esri/themes/light/main.css";
+
+const SlideContainer = styled.div<{ height: string }>`
+  height: ${(props) => props.height};
+  text-align: center;
+`;
+
+const SlideHeader = styled.div`
+  font-size: 2.5em;
+  color: #237cbd;
+`;
+
+const SplashModal = () => (
+  <CalciteModal active aria-labelledby="modal-title" id="splashModal">
+    <div slot="header">Choose a community!</div>
+    <div slot="content">
+      Use the Area button to select a group of marine animal species! You will need to create two
+      areas in order to compare the Species Diversity Index.
+    </div>
+    <CalciteButton
+      appearance="outline"
+      onClick={() =>
+        ((document.getElementById("splashModal") as HTMLCalciteModalElement).active = false)
+      }
+      slot="secondary"
+      width="full"
+    >
+      Okay
+    </CalciteButton>
+  </CalciteModal>
+);
 
 const MapSwiperDeck: React.FC<{ height: string }> = ({ height }): JSX.Element => {
   const [isModalDisplayed, showModal] = React.useState(false);
-
-  const SlideContainer = styled.div<{ height: string }>`
-    height: ${(props) => props.height};
-    text-align: center;
-  `;
-
-  const SlideHeader = styled.div`
-    font-size: 2.5em;
-    color: #237cbd;
-  `;
-
-  const SplashModal = () => (
-    <CalciteModal active aria-labelledby="modal-title" id="splashModal">
-      <div slot="header">Choose a community!</div>
-      <div slot="content">
-        Use the Area button to select a group of marine animal species! You will need to create two
-        areas in order to compare the Species Diversity Index.
-      </div>
-      <CalciteButton
-        appearance="outline"
-        onClick={() =>
-          ((document.getElementById("splashModal") as HTMLCalciteModalElement).active = false)
-        }
-        slot="secondary"
-        width="full"
-      >
-        Okay
-      </CalciteButton>
-    </CalciteModal>
-  );
 
   return (
     <ErrorBoundary>
