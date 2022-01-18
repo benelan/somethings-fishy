ca0261486a8dd3ef47b9edb4f694702a21c008db
Ben Elan <benelan@protonmail.com>
2022-01-17 16:40:28 -0800

    feat(info): add styled component for map  info rows

--- src/components/maps/BioMapInfo.tsx
+++ src/components/maps/BioMapInfo.tsx
@@ -4,29 +4,44 @@ import { CalcitePanel } from "@esri/calcite-components-react";
 import "./BioMap.css";
 import styled from "styled-components";
 
-const Panel = styled.div`
-  width: 50%;
+const InfoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const InfoItem = styled.div`
+  flex: 0 44%;
+  margin: 3%;
 `;
 
 export default (): JSX.Element => (
-  <Panel as={CalcitePanel} heading="Shannon-Wiener Index">
-    <div id="info-panel-content">
-      <p className="p-math">
-        `H = &#8212;<span className="span-math">&Sigma;</span>P<sub>i</sub> ln(P<sub>i</sub>)`
-      </p>
-      <p>
-        <b>
-          P<sub>i</sub>
-        </b>{" "}
-        - the proportion of individuals in a species
-        <br />
-        <b>H</b> - the index
-      </p>
-      <p>
-        The <b>Shannon-Wiener Index</b> is a diversity index widely used to compare species
-        diversity. This index is not meant to be a measurement of diversity, but a tool to compare
-        diversity between one or more communities of species.
-      </p>
-    </div>
-  </Panel>
+  <InfoContainer>
+    <InfoItem as={CalcitePanel} heading="Shannon-Wiener Index">
+      <div id="info-panel-content">
+        <p className="p-math">
+          `H = &#8212;<span className="span-math">&Sigma;</span>P<sub>i</sub> ln(P<sub>i</sub>)`
+        </p>
+        <p>
+          <b>
+            P<sub>i</sub>
+          </b>{" "}
+          - the proportion of individuals in a species
+          <br />
+          <b>H</b> - the index
+        </p>
+        <p>
+          The <b>Shannon-Wiener Index</b> is a diversity index widely used to compare species
+          diversity. This index is not meant to be a measurement of diversity, but a tool to compare
+          diversity between one or more communities of species.
+        </p>
+      </div>
+    </InfoItem>
+
+    <InfoItem>
+      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+      blah blah blah blah blah blah blah blah blah blah blah blah blah dur dur dur dur dur dur dur
+      dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur dur
+    </InfoItem>
+  </InfoContainer>
 );
