67f8a8075b21cc09a101e482c7494930938eace7
Ben Elan <benelan@users.noreply.github.com>
2022-01-18 00:05:59 -0800

    refactor: reorganize maps (#55)
    * fix(maps): move bio to bottom due to scroll issues
    
    * move maps to their own directory
    
    * cleanup

--- src/components/maps/MapDeck.tsx
+++ src/components/maps/MapDeck.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import LazyLoad from "react-lazyload";
-import BioMap from "./BioMap";
-import BioMapInfo from "./BioMapInfo";
-import RarityMap from "./RarityMap";
-import RarityMapInfo from "./RarityMapInfo";
-import CollectionMap from "./CollectionMap";
-import CollectionMapInfo from "./CollectionMapInfo";
-import SSTMap from "./SSTMap";
-import SSTMapInfo from "./SSTMapInfo";
+import BioMap from "./bio/BioMap";
+import BioMapInfo from "./bio/BioMapInfo";
+import RarityMap from "./rarity/RarityMap";
+import RarityMapInfo from "./rarity/RarityMapInfo";
+import CollectionMap from "./collection/CollectionMap";
+import CollectionMapInfo from "./collection/CollectionMapInfo";
+import SSTMap from "./sst/SSTMap";
+import SSTMapInfo from "./sst/SSTMapInfo";
 import ErrorBoundary from "../ErrorBoundary";
 
 import "@arcgis/core/assets/esri/themes/light/main.css";
@@ -46,18 +46,8 @@ const MapDeck: React.FC<{ height: string }> = ({ height }): JSX.Element => {
 
       <ErrorBoundary>
         <LazyLoad height={height} offset={100}>
-          <MapHeader>Biodiversity Map</MapHeader>
-          <MapInfo as={BioMapInfo} />
-          <MapContainer height={height}>
-            <BioMap />
-          </MapContainer>
-        </LazyLoad>
-      </ErrorBoundary>
-
-      <ErrorBoundary>
-        <LazyLoad height={height} offset={100}>
           <MapHeader>Marine Species Rarity vs. Protected Area Map</MapHeader>
-          <MapInfo as={RarityMapInfo} />
+          <RarityMapInfo />
           <MapContainer height={height}>
             <RarityMap />
           </MapContainer>
@@ -70,6 +60,16 @@ const MapDeck: React.FC<{ height: string }> = ({ height }): JSX.Element => {
           <MapInfo as={CollectionMapInfo} />
           <MapContainer height={height}>
             <CollectionMap />
+          </MapContainer>
+        </LazyLoad>
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <LazyLoad height={height} offset={100}>
+          <MapHeader>Biodiversity Map</MapHeader>
+          <MapInfo as={BioMapInfo} />
+          <MapContainer height={height}>
+            <BioMap />
           </MapContainer>
         </LazyLoad>
       </ErrorBoundary>
