6cc2309da291f9171c7af930e851210b718f942b
Ben Elan <benelan@users.noreply.github.com>
2022-01-18 01:18:46 -0800

    feat(info): add items to cards (#58)
    * feat(info): add items to cards
    
    * add rel to links

--- src/components/maps/collection/CollectionMapInfo.tsx
+++ src/components/maps/collection/CollectionMapInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@esri/calcite-components/dist/components/calcite-link";
-import { CalciteLink } from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-card";
+import { CalciteLink, CalciteCard } from "@esri/calcite-components-react";
 import styled from "styled-components";
 
 const InfoContainer = styled.div`
@@ -14,6 +15,8 @@ const InfoContainer = styled.div`
 const InfoItem = styled.div`
   flex: 1 1 40vw;
   margin: 1rem;
+  --calcite-font-size--2: 1em;
+  --calcite-font-size--1: 1.3em;
 `;
 
 const SourceLink = styled.a`
@@ -22,35 +25,36 @@ const SourceLink = styled.a`
 `;
 
 export default (): JSX.Element => (
-  <>
-    <h2 style={{ textAlign: "center", marginLeft: "-1rem", marginRight: "-1rem" }}>
-      Marine Debris: If you see something, pick it up and/or let others know!
-    </h2>
-    <InfoContainer>
-      <InfoItem>
-        <p>
-          According to National Ocean Service, marine debris is defined as any persistent solid
-          material that is manufactured or processed and directly or indirectly, intentionally or
-          unintentionally, disposed of or abandoned into the marine environment or the Great Lakes.
-          There is no part of the world left untouched by debris and its impacts. It is one of the
-          most widespread pollution problems facing the world's ocean and waterways.{"   "}
-          <SourceLink
-            as={CalciteLink}
-            href="https://oceanservice.noaa.gov/hazards/marinedebris/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            source
-          </SourceLink>
-        </p>
-        <p>
-          This mapping application shows marine debris data points that we collected via ArcGIS
-          Survey123. Click on the points to see further details (debris type, photo, etc.) in a
-          pop-up. Click on other places on the map to see demographic data of the area (if they are
-          available).
-        </p>
-      </InfoItem>
-      <InfoItem>
+  <InfoContainer>
+    <InfoItem>
+      <CalciteCard>
+        <h3 slot="title">
+          Marine Debris: If you see something, pick it up and/or let others know!
+        </h3>
+        According to National Ocean Service, marine debris is defined as any persistent solid
+        material that is manufactured or processed and directly or indirectly, intentionally or
+        unintentionally, disposed of or abandoned into the marine environment or the Great Lakes.
+        There is no part of the world left untouched by debris and its impacts. It is one of the
+        most widespread pollution problems facing the world's ocean and waterways.{"   "}
+        <SourceLink
+          as={CalciteLink}
+          href="https://oceanservice.noaa.gov/hazards/marinedebris/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          source
+        </SourceLink>
+      </CalciteCard>
+      <CalciteCard style={{ marginTop: "2rem" }}>
+        \<h3 slot="title">About the map</h3>
+        This mapping application shows marine debris data points that we collected via ArcGIS
+        Survey123. Click on the points to see further details (debris type, photo, etc.) in a
+        pop-up. Click on other places on the map to see demographic data of the area (if they are
+        available).
+      </CalciteCard>
+    </InfoItem>
+    <InfoItem>
+      <CalciteCard>
         <div style={{ textAlign: "center" }}>
           <a href="https://arcg.is/1T8nfr0" rel="noopener noreferrer" target="_blank">
             <img
@@ -67,7 +71,7 @@ export default (): JSX.Element => (
             </CalciteLink>
           </p>
         </div>
-      </InfoItem>
-    </InfoContainer>
-  </>
+      </CalciteCard>
+    </InfoItem>
+  </InfoContainer>
 );
--- src/components/maps/sst/SSTMapInfo.tsx
+++ src/components/maps/sst/SSTMapInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@esri/calcite-components/dist/components/calcite-link";
-import { CalciteLink } from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-card";
+import { CalciteLink, CalciteCard } from "@esri/calcite-components-react";
 import styled from "styled-components";
 
 const InfoContainer = styled.div`
@@ -14,7 +15,10 @@ const InfoContainer = styled.div`
 const InfoItem = styled.div`
   flex: 1 1 40vw;
   margin: 1rem;
+  --calcite-font-size--2: 1em;
+  --calcite-font-size--1: 1.3em;
 `;
+
 const Link = styled.a`
   font-size: 0.7em;
   vertical-align: super;
@@ -22,7 +26,7 @@ const Link = styled.a`
 
 export default (): JSX.Element => (
   <InfoContainer>
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", margin: "1rem" }}>
       <p>
         <b>
           <i>
@@ -34,121 +38,124 @@ export default (): JSX.Element => (
       </p>
     </div>
     <InfoItem>
-      <h3>
-        <b>Effects of Global Sea Temperatures Rising</b>
-      </h3>
-      <img
-        alt="Effects of global sea temperatures rising."
-        src="/img/GlobalSurfaceTemperatureRising.png"
-        style={{ height: "auto", width: "100%", maxWidth: "900px" }}
-      />
-      <p>
-        {"   "}
-        <Link
-          as={CalciteLink}
-          href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          source
-        </Link>
-      </p>
-      <h3>
-        <b>Why is the rise in sea temperature important?</b>
-      </h3>
-      <p>
-        Rising sea temperature leads to a reduction in the amount of oxygen dissolved in the ocean
-        and sea level rise, which causes negative effects for marine species and ecosystems.
-        Additionally, this harms fundamental benefits that humans get from the ocean.
-      </p>
-    </InfoItem>
-    <InfoItem>
-      <h3>
-        <b>How Humans Impact Sea Surface Temperature</b>
-      </h3>
-      <ul>
-        <li>
-          The ocean absorbs most of the excess heat from greenhouse gas emissions, leading to rising
-          ocean temperatures.{"   "}
-          <Link
-            as={CalciteLink}
-            href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            source
-          </Link>
-        </li>
-        <li>
-          Increasing ocean temperatures affect marine species and ecosystems. Rising temperatures
-          cause coral bleaching and the loss of breeding grounds for marine fishes and mammals.
-          {"   "}
-          <Link
-            as={CalciteLink}
-            href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            source
-          </Link>
-        </li>
-        <li>
-          Rising ocean temperatures also affect the benefits humans derive from the ocean -
-          threatening food security, increasing the prevalence of diseases, and causing extreme
-          weather events and loss of coastal protection.{"   "}
-          <Link
-            as={CalciteLink}
-            href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            source
-          </Link>
-        </li>
-      </ul>
-      <h3>
-        <b>Trends in Global Emissions</b>
-      </h3>
-      <div style={{ alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+      <CalciteCard>
+        <h3 slot="title">Effects of Global Sea Temperatures Rising</h3>
         <img
-          alt="Trends of Global Emissions."
-          src="/img/EmissionsTrends.png"
-          style={{ height: "auto", width: "100%", maxWidth: "500px" }}
+          alt="Effects of global sea temperatures rising."
+          src="/img/GlobalSurfaceTemperatureRising.png"
+          style={{ height: "auto", width: "100%", maxWidth: "900px" }}
         />
-        <p style={{ textAlign: "center" }}>
+        <p>
           {"   "}
           <Link
             as={CalciteLink}
-            href="https://www.epa.gov/ghgemissions/global-greenhouse-gas-emissions-data"
+            href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
             rel="noopener noreferrer"
             target="_blank"
           >
             source
           </Link>
         </p>
-      </div>
+      </CalciteCard>
+
+      <CalciteCard style={{ marginTop: "2rem" }}>
+        <h3 slot="title">Why is the rise in sea temperature important?</h3>
+        <p>
+          Rising sea temperature leads to a reduction in the amount of oxygen dissolved in the ocean
+          and sea level rise, which causes negative effects for marine species and ecosystems.
+          Additionally, this harms fundamental benefits that humans get from the ocean.
+        </p>
+      </CalciteCard>
     </InfoItem>
-    <div>
-      <h3>How to use the Application</h3>
-      The Sea Surface Temperature Map Application allows you to visualize how the SST has fluctuated
-      over the last decade.
-      <ol>
-        <li>
-          <p>Zoom to an area of interest to visualize Sea Surface Temperature (SST).</p>
-        </li>
-        <li>
-          <p>
-            Click the "Play" button on the Time Slider to visualize the change in SST from March 31,
-            2008 to present.
+    <InfoItem>
+      <CalciteCard>
+        <h3 slot="title">How Humans Impact Sea Surface Temperature</h3>
+        <ul>
+          <li>
+            The ocean absorbs most of the excess heat from greenhouse gas emissions, leading to
+            rising ocean temperatures.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </li>
+          <li>
+            Increasing ocean temperatures affect marine species and ecosystems. Rising temperatures
+            cause coral bleaching and the loss of breeding grounds for marine fishes and mammals.
+            {"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </li>
+          <li>
+            Rising ocean temperatures also affect the benefits humans derive from the ocean -
+            threatening food security, increasing the prevalence of diseases, and causing extreme
+            weather events and loss of coastal protection.{"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.iucn.org/resources/issues-briefs/ocean-warming"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
+          </li>
+        </ul>
+      </CalciteCard>
+      <CalciteCard style={{ marginTop: "2rem" }}>
+        <h3 slot="title">Trends in Global Emissions</h3>
+        <div style={{ alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+          <img
+            alt="Trends of Global Emissions."
+            src="/img/EmissionsTrends.png"
+            style={{ height: "auto", width: "100%", maxWidth: "500px" }}
+          />
+          <p style={{ textAlign: "center" }}>
+            {"   "}
+            <Link
+              as={CalciteLink}
+              href="https://www.epa.gov/ghgemissions/global-greenhouse-gas-emissions-data"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              source
+            </Link>
           </p>
-        </li>
-        <li>
-          <p>
-            Alternatively, click and drag the marker on the slider to see a specific date within the
-            time range.
-          </p>
-        </li>
-      </ol>
-    </div>
+        </div>
+      </CalciteCard>
+    </InfoItem>
+    <InfoItem>
+      <CalciteCard>
+        <h3 slot="title">How to use the Application</h3>
+        The Sea Surface Temperature Map Application allows you to visualize how the SST has
+        fluctuated over the last decade.
+        <ol>
+          <li>
+            <p>Zoom to an area of interest to visualize Sea Surface Temperature (SST).</p>
+          </li>
+          <li>
+            <p>
+              Click the "Play" button on the Time Slider to visualize the change in SST from March
+              31, 2008 to present.
+            </p>
+          </li>
+          <li>
+            <p>
+              Alternatively, click and drag the marker on the slider to see a specific date within
+              the time range.
+            </p>
+          </li>
+        </ol>
+      </CalciteCard>
+    </InfoItem>
   </InfoContainer>
 );
